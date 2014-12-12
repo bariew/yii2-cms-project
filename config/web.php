751bbcbdecbc4bfca385b33795fb186922bb7449
Pavel Bariev <bariew@yandex.ru>
2014-12-12 18:48:25 +0400

    removed extra route rules

--- config/web.php
+++ config/web.php
@@ -17,8 +17,6 @@
             'showScriptName'        => false,
             'enableStrictParsing'   => true,
             'rules' => [
-                '<_a>'    => 'site/<_a>',
-                '<_c>/<_a>'=>'<_c>/<_a>',
                 '<_m>/<_c>/<_a>' => '<_m>/<_c>/<_a>',
                 '/'                 => 'site/index',
             ],
