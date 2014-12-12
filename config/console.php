4a33355272d3618d8d0d76a4661c131530df8748
Pavel Bariev <bariew@yandex.ru>
2014-12-12 14:24:57 +0400

    composer dependencies versions

--- config/console.php
+++ config/console.php
@@ -1,29 +1,10 @@
 <?php
-
-Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
-$mainConfigPath = __DIR__ . '/web.php';
-$mainConfig = file_exists($mainConfigPath) ? require $mainConfigPath : [];
-
+$mainConfig = file_exists(__DIR__ . '/web.php') ? require __DIR__ . '/web.php' : [];
 return [
-    'id' => 'basic-console',
+    'id' => 'console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'extensions'=> require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'authManager'   => [
-            'class' => '\yii\rbac\DbManager'
-        ],
         'db'    => $mainConfig['components']['db']
     ],
     'modules' => $mainConfig['modules'],
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,24 +1,38 @@
 <?php
+/**
+ * SiteController class file.
+ * @copyright (c) 2014, Bariew
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
 
 namespace app\controllers;
 
 use yii\web\Controller;
 
+/**
+ * Simple base controller.
+ *
+ * @author Pavel Bariev <bariew@yandex.ru>
+ */
 class SiteController extends Controller
 {
+    /**
+     * Controller additional actions.
+     * @return array available actions.
+     */
     public function actions()
     {
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
         ];
     }
-    
+
+    /**
+     * Renders index page.
+     * @return string view.
+     */
     public function actionIndex()
     {
         return $this->render('index');
